2e8e98442a241e8a38cf90296f117eb232d9e78a
Felix Freidman <felixfreidman@gmail.com>
2021-09-29 16:22:46 +0800

    4:22PM Commit Something like git init hehe

---
+++ build/assets/js/main.js
@@ -0,0 +1,52 @@
+//   $('#btn-menu-mob').click(function(e) {
+//       e.preventDefault();
+//       $('.header__svg-mob').addClass('is-active');
+//       $('#menu-mobile').animate({ 
+//         right: '0px' 
+//         }, 300);
+//       $('#menu-mobile').animate({ 
+//         right: '0px' 
+//         }, 300);
+//     $('body').css('overflow', 'hidden');
+//     $('.page').animate({ 
+//         right: '190px' 
+//     }, 200); 
+// });
+// $('.menu-mobile__svg-close').click(function(e) {
+//     e.preventDefault();
+//     $('.header__svg-mob').removeClass('is-active');
+//     $('#menu-mobile').animate({ 
+//       right: '-207px' 
+//   }, 300);
+//   $('body').css('overflow', 'auto');
+//   $('.page').animate({ 
+//       right: '0px' 
+//   }, 200); 
+// });
+// // header-swiper
+// var swiper = new Swiper('#main-swiper', {
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next--header',
+//     prevEl: '.swiper-button-prev--header',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//     clickable: true,
+//   },
+//   loop: true,
+//   // autoplay: {
+//   //   delay: 2300,
+//   // },
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   speed: 800,
+//   watchSlidesProgress: true,
+//   watchVisibility: true,
+//   disableOnInteraction: true,
+// });
+"use strict";
