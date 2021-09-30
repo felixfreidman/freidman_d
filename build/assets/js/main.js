cb9d69831377e02526c841c39cc76846e563d063
Felix Freidman <felixfreidman@gmail.com>
2021-09-30 12:38:03 +0800

    12:37PM Commit Сделал все страницы Адаптив и на WP

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -4,13 +4,16 @@ var menuDefault = document.getElementById("header-main");
 var menuOpened = document.getElementById("header-opened");
 var toggleOpen = document.getElementById("toggle-open");
 var toggleClose = document.getElementById("toggle-close");
+var layerHeader = document.getElementById("blur-header-layer");
 toggleOpen.addEventListener("click", function () {
   menuOpened.classList.toggle("header-js--hidden");
   menuDefault.classList.toggle("header-js--hidden");
+  layerHeader.classList.toggle("header-js--hidden");
 });
 toggleClose.addEventListener("click", function () {
   menuDefault.classList.toggle("header-js--hidden");
   menuOpened.classList.toggle("header-js--hidden");
+  layerHeader.classList.toggle("header-js--hidden");
 });
 var aboutSlider = new Swiper("#home-swiper", {
   direction: "horizontal",
--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -2,14 +2,18 @@ var menuDefault = document.getElementById("header-main");
 var menuOpened = document.getElementById("header-opened");
 var toggleOpen = document.getElementById("toggle-open");
 var toggleClose = document.getElementById("toggle-close");
+var layerHeader = document.getElementById("blur-header-layer");
 toggleOpen.addEventListener("click", () => {
     menuOpened.classList.toggle("header-js--hidden");
     menuDefault.classList.toggle("header-js--hidden");
+    layerHeader.classList.toggle("header-js--hidden");
 });
 toggleClose.addEventListener("click", () => {
     menuDefault.classList.toggle("header-js--hidden");
     menuOpened.classList.toggle("header-js--hidden");
+    layerHeader.classList.toggle("header-js--hidden");
 });
+
 var aboutSlider = new Swiper("#home-swiper", {
     direction: "horizontal",
     fadeEffect: { crossFade: true },
